e48d959731f0cd7e96a470e2276794f396d60000
kali-om <kali-om@kali>
2024-10-23 16:30:48 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: parseInt(process.env.VITE_PORT) || 3000, // Fallback to port 3000 if VITE_PORT is not set
+    port: parseInt(process.env.VITE_PORT) || 5010, // Fallback to port 3000 if VITE_PORT is not set
   },
 })
